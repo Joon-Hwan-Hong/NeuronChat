59c0f3229a4d5809f841e4744cd1d8b2bbef9af8
Joon Hwan Hong <49882424+Joon-Hwan-Hong@users.noreply.github.com>
2025-06-06 03:29:02 -0400

    Add network analysis utilities and embeddings

--- python/neuronchat/__init__.py
+++ python/neuronchat/__init__.py
@@ -4,13 +4,33 @@ from .core import (
     NeuronChat,
     net_aggregation,
     computeNetSimilarityPairwise_Neuron,
+    computeNetSimilarity_Neuron,
+    selectK_Neuron,
+    identifyCommunicationPatterns_Neuron,
+    rankNet_Neuron,
+    compareInteractions_Neuron,
 )
 from .db import load_interactionDB, update_interactionDB
+from .visualization import (
+    netVisual_circle_neuron,
+    heatmap_single,
+    netVisual_embedding_Neuron,
+    netVisual_embeddingPairwise_Neuron,
+)
 
 __all__ = [
     "NeuronChat",
     "net_aggregation",
     "computeNetSimilarityPairwise_Neuron",
+    "computeNetSimilarity_Neuron",
+    "selectK_Neuron",
+    "identifyCommunicationPatterns_Neuron",
+    "rankNet_Neuron",
+    "compareInteractions_Neuron",
+    "netVisual_circle_neuron",
+    "heatmap_single",
+    "netVisual_embedding_Neuron",
+    "netVisual_embeddingPairwise_Neuron",
     "load_interactionDB",
     "update_interactionDB",
 ]
